8f02bfac8e2a6da2647c33d36b676efdd3a42ee1
Denis <deniskozlov@live.ru>
2015-11-20 18:11:39 +0300

    Initial commit with simple command

---
+++ AppiumTestApp/ProgramTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+
+namespace AppiumTestApp
+{
+    [TestClass]
+    public class ProgramTest
+    {
+
+        private const string AppPath = @"C:/files/Telegram_3.2.6.apk";
+        private const string DeviceAddress = "http://127.0.0.1:4723/wd/hub";
+
+        private AndroidDriver<AppiumWebElement> _driver;
+
+        [TestInitialize]
+        public void beforeAll()
+        {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+
+            capabilities.SetCapability("deviceName", "Google Nexus 6 - 5.0.0 - API 21 - 1400x2560");
+            capabilities.SetCapability("platformName", "Android");
+            capabilities.SetCapability("app", AppPath);
+
+            _driver = new AndroidDriver<AppiumWebElement>(
+                                                    new Uri(DeviceAddress),
+                                                    capabilities);
+        }
+
+        [TestCleanup]
+        public void afterAll()
+        {
+            _driver.Quit();
+        }
+
+        [TestMethod]
+        public void AppiumDriverMethodsTestCase()
+        {
+            AppiumWebElement el = _driver.FindElementByAndroidUIAutomator("new UiSelector().enabled(true)");
+
+            el.SendKeys("abc");
+            Assert.AreEqual(el.Text,"abc","Проверка не пройдена");
+        }
+    }
+
+}
+
---
+++ FirstAppiumApp/FirstAppiumDriver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+
+namespace FirstAppiumApp
+{
+    [TestFixture()]
+    public class ProgramTest
+    {
+        private AppiumDriver<AppiumWebElement> driver;
+
+        public void beforeAll()
+        {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+
+            capabilities.SetCapability("deviceName","");
+            capabilities.SetCapability("platformName","Android");
+            capabilities.SetCapability("app",PATH);
+
+            driver = new AndroidDriver<AppiumWebElement>(new Uri("http://127.0.0.1:4723/wh/hub"), capabilities);
+        }
+
+        [TestFixtureDown]
+        public void afterAll()
+        {
+            driver.Quit();
+        }
+
+        public void AppiumDriverMethodsTestCase()
+        {
+            AppiumWebElement el = driver.FindElementsByAndroidUIAutomator("new UiSelector().enables(true)");
+
+            el.SendKeys("abc");
+            Assert.
+        }
+    }
+
+}
